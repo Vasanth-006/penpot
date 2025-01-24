ec59d1c8da23dcbadcf439425eb81cf7d88753e1
vasanth-006 <vasanth4013@gmail.com>
2025-01-24 12:01:10 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import PenpotImage from "../assets/Desktop.jpg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <img src={PenpotImage} alt="Penpot Design" width="100%" />
+      console.log("vasanth");
+    </div>
+  );
+}
