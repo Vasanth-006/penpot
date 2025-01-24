7c8b8b2d15c4900aaa8a3013775f35c1d2777aa2
vasanth-006 <vasanth4013@gmail.com>
2025-01-24 12:07:59 +0000

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <img src={PenpotImage} alt="Penpot Design" width="100%" />
-      console.log("vasanth");
+      console.log("dhanush")
     </div>
   );
 }
